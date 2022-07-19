ae506b65534b8166ffc209923b780625e87b3fe4
denizedaza <dazadenize@gmail.com>
2022-07-19 13:20:32 -0400

    add and implement class modules for meetup details

--- components/meetups/MeetupDetail.tsx
+++ components/meetups/MeetupDetail.tsx
@@ -1,12 +1,15 @@
+import classes from "./MeetupDetail.module.css";
+
 function MeetupDetail(props: any) {
   const { image, title, address, description } = props;
 
   return (
-    <>
+    <section className={classes.detail}>
+      <img src={image} alt={title}></img>
       <h1>{title}</h1>
       <address>{address}</address>
       <p>{description}</p>
-    </>
+    </section>
   );
 }
 
